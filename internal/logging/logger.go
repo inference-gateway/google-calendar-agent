4f5a8bc4330851779614f1d2f23ea8e129c5e8e5
Eden Reich <eden.reich@gmail.com>
2025-06-14 15:16:39 +0000

    refactor: Remove unnecessary comments in logger configuration
    Signed-off-by: Eden Reich <eden.reich@gmail.com>

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -11,25 +11,21 @@ import (
 func NewLogger(cfg config.LoggingConfig) (*zap.Logger, error) {
 	var zapConfig zap.Config
 
-	// Set up config based on environment
 	if cfg.Format == "console" {
 		zapConfig = zap.NewDevelopmentConfig()
 	} else {
 		zapConfig = zap.NewProductionConfig()
 	}
 
-	// Set log level
 	level, err := zap.ParseAtomicLevel(cfg.Level)
 	if err != nil {
 		return nil, fmt.Errorf("invalid log level '%s': %w", cfg.Level, err)
 	}
 	zapConfig.Level = level
 
-	// Configure caller information
 	zapConfig.DisableCaller = !cfg.EnableCaller
 	zapConfig.DisableStacktrace = !cfg.EnableStacktrace
 
-	// Set output paths based on configuration
 	switch cfg.Output {
 	case "stdout":
 		zapConfig.OutputPaths = []string{"stdout"}
@@ -38,7 +34,6 @@ func NewLogger(cfg config.LoggingConfig) (*zap.Logger, error) {
 	case "":
 		zapConfig.OutputPaths = []string{"stdout"}
 	default:
-		// Assume it's a file path
 		zapConfig.OutputPaths = []string{cfg.Output}
 	}
 
